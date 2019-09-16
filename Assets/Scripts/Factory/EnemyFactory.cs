0f763a4bdd9833766e50f75f4916f2143267191d
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-16 22:57:11 +1200

    feat: Factory to handle instantiation of enemies

---
+++ Assets/Scripts/Factory/EnemyFactory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType
+{
+    Drone,
+    Sniper
+}
+
+public class EnemyFactory : MonoBehaviour
+{
+    private ICopyable copyable;
+
+    public ICopyable GetEnemy(EnemyType enemyType)
+    {
+        switch (enemyType)
+        {
+            case EnemyType.Drone:
+                return Instantiate(Resources.Load("Drone", typeof(Enemy)) as Enemy);;
+            case EnemyType.Sniper:
+                return Instantiate(Resources.Load("Sniper", typeof(Enemy)) as Enemy);
+        }
+        return null;
+    }
+    public ICopyable GetEnemy(Enemy enemy, bool usePrefab = true)
+    {
+        copyable = null;
+        if (usePrefab)
+        {
+            if (enemy.GetType() == typeof(Drone))
+            {
+                copyable = enemy.Copy("Drone");
+            }
+            else if (enemy.GetType() == typeof(Sniper))
+            {
+                copyable = enemy.Copy("Sniper");
+            }
+        }
+        else
+        {
+            copyable = enemy.Copy();
+        }
+        
+        return copyable as Enemy;
+    }
+}
