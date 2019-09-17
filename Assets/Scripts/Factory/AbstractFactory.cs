5d0d844f32c42ee0e59af32eee4b5b8da00b868e
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-18 00:10:01 +1200

    implement AbstractFactory

---
+++ Assets/Scripts/Factory/AbstractFactory.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Scripts.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class AbstractFactory
+{
+    public abstract IEnemy GetEnemy();
+
+    public abstract IAlly GetAlly();
+}
