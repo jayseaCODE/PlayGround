4e90763ebe02e105f2269778b8280683f69adabd
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-18 20:10:03 +1200

    refactor Spawner class

---
+++ Assets/Scripts/PrototypePattern/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    private AbstractFactory factory;
+    public Enemy SpawnEnemy(EnemyType enemyType)
+    {
+        factory = new EnemyFactory();
+        return factory.GetEnemy(enemyType) as Enemy;
+    }
+
+    public Ally SpawnAlly(AllyType allyType)
+    {
+        factory = new AllyFactory();
+        return factory.GetAlly(allyType) as Ally;
+    }
+}
