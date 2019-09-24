ff03d1c08a5b1c5035611195b5c454b1f5f87a7e
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-24 22:53:25 +1200

    feat: behavior implementation using Strategy pattern

---
+++ Assets/Scripts/PrototypePattern/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Demonstration of the Strategy pattern: Being able to switch behavior dynamically and apply it
+/// </summary>
+public abstract class Unit : MonoBehaviour
+{
+    protected IUnitBehavior unitBehavior;
+
+    public void ApplyBehavior()
+    {
+        unitBehavior.Command();
+    }
+
+    public void SetBehavior(IUnitBehavior behavior)
+    {
+        unitBehavior = behavior;
+    }
+}
---
+++ Assets/Scripts/UnitBehavior/Attack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : IUnitBehavior
+{
+    public void Command()
+    {
+        Debug.Log("Unit is now attacking.");
+    }
+}
