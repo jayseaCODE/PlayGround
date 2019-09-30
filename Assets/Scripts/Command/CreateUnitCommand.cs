1340b0019c0a4de2fd2e5d3dadde9e739c24f45f
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-30 19:18:09 +1300

    feat: Command class that allows a Commander to invoke them

---
+++ Assets/Scripts/Command/CreateUnitCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateUnitCommand : UnitCommand
+{
+    public CreateUnitCommand(Unit unit) : base(unit)
+    {
+    }
+    public override void Execute()
+    {
+        _unit.Copy();
+    }
+}
---
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UnitCommand
+{
+    protected Unit _unit;
+
+    public UnitCommand(Unit unit)
+    {
+        _unit = unit;
+    }
+
+    public abstract void Execute();
+}
