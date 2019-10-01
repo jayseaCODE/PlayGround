bdb57dfeb3ef99e0b51da73f3fe4a773ba75bb20
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-01 22:17:24 +1300

    feat: DeleteUnitCommand

---
+++ Assets/Scripts/Command/DeleteUnitCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteUnitCommand : UnitCommand
+{
+    public DeleteUnitCommand(Unit unit) : base(unit)
+    {
+
+    }
+
+    public override void Execute()
+    {
+        Object.Destroy(_unit);
+    }
+}
