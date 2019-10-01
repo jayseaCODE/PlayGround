a466a64bf1f427a715014755b41cc6954999a5b9
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-01 22:56:20 +1300

    feat: Commander able to save and execute commands

---
+++ Assets/Scripts/Command/Commander.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commander
+{
+    protected Stack<UnitCommand> unitCommandsToExecute;
+    protected Stack<UnitCommand> unitCommandsExecuted;
+
+    public void SetUnitCommand(UnitCommand unitCommand)
+    {
+        unitCommandsToExecute.Push(unitCommand);
+    }
+
+    public void ExecuteUnitCommand()
+    {
+        UnitCommand unitCommand = unitCommandsToExecute.Pop();
+        unitCommand.Execute();
+        unitCommandsExecuted.Push(unitCommand);
+    }
+}
