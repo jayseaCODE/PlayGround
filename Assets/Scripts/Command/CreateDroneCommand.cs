48b5fa536fb86c23c918a681f7dd4d935b70bec7
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-09 21:56:37 +1300

    feat: DroneCommand which Creates a Drone and randomly positions it

---
+++ Assets/Scripts/Command/CreateDroneCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDroneCommand : UnitCommand
+{
+    public CreateDroneCommand(Drone unit) : base(unit)
+    {
+    }
+    public override void Execute()
+    {
+        Drone copiedDrone = _unit.Copy() as Drone;
+        Vector3 randomPosition = new Vector3(Random.Range(-10, 10), Random.Range(5, 20), Random.Range(-10, 10));
+        copiedDrone.transform.SetPositionAndRotation(randomPosition, new Quaternion());
+    }
+}
