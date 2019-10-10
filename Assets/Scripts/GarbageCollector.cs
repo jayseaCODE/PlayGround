17b0b9278143933bd4e38e9f63dab4e865b21037
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-10 19:44:27 +1300

    feat: GarbageCollector to destroy fallen objects

---
+++ Assets/Scripts/GarbageCollector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCollector : MonoBehaviour
+{
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
