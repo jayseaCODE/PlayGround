2a9eacb71557d0dcabbfa67f4e8f6613a79b3cbd
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-12 21:08:39 +1300

    feat: CreateCommand for Sniper enemy entity

---
+++ Assets/Scripts/Command/CreateSniperCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSniperCommand : UnitCommand
+{
+    public CreateSniperCommand(Sniper unit) : base(unit)
+    {
+    }
+    public override void Execute()
+    {
+        Sniper copiedSniper = _unit.Copy() as Sniper;
+        Vector3 randomPosition = new Vector3(Random.Range(-10, 10), Random.Range(5, 20), Random.Range(-10, 10));
+        copiedSniper.transform.SetPositionAndRotation(randomPosition, new Quaternion());
+    }
+}
