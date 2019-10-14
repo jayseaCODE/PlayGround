c0fb7f0c7e0127d3292cd312c6f7e4c492c9c54b
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-14 17:48:43 +1300

    feat: CreateCommand for Medix ally entity

---
+++ Assets/Scripts/Command/CreateMedicCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMedicCommand : UnitCommand
+{
+    public CreateMedicCommand(Medic unit) : base(unit)
+    {
+    }
+    public override void Execute()
+    {
+        Medic copiedDrone = _unit.Copy() as Medic;
+        Vector3 randomPosition = new Vector3(Random.Range(-10, 10), Random.Range(5, 20), Random.Range(-10, 10));
+        copiedDrone.transform.SetPositionAndRotation(randomPosition, new Quaternion());
+    }
+}
