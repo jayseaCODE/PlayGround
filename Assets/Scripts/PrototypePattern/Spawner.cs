04996e7ff5b46cf610cdd5d2209234bb6c3b7b0b
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-16 22:15:19 +1300

    refactor: Spawner to make use of Singleton factories

--- Assets/Scripts/PrototypePattern/Spawner.cs
+++ Assets/Scripts/PrototypePattern/Spawner.cs
@@ -4,41 +4,13 @@ using UnityEngine;
 
 public class Spawner : Singleton<Spawner>
 {
-    IFactory _allyFactory;
-    IFactory _enemyFactory;
-    bool _searchedForEnemyFactory;
-    bool _searchedForAllyFactory;
-    public Spawner()
-    {
-        _searchedForEnemyFactory = false;
-        _searchedForAllyFactory = false;
-    }
     public Enemy SpawnEnemy(EnemyType enemyType)
     {
-        if (!_searchedForEnemyFactory)
-        {
-            _enemyFactory = FindObjectOfType<EnemyFactory>();
-            _searchedForEnemyFactory = true;
-        }
-        if (_enemyFactory == null)
-        {
-            _enemyFactory = gameObject.AddComponent(typeof(EnemyFactory)) as EnemyFactory;
-        }
-
-        return _enemyFactory.GetEnemy(enemyType) as Enemy;
+        return EnemyFactory.Instance.GetEnemy(enemyType) as Enemy;
     }
 
     public Ally SpawnAlly(AllyType allyType)
     {
-        if (!_searchedForAllyFactory)
-        {
-            _allyFactory = FindObjectOfType<AllyFactory>();
-            _searchedForAllyFactory = true;
-        }
-        if (_allyFactory == null)
-        {
-            _allyFactory = gameObject.AddComponent(typeof(AllyFactory)) as AllyFactory;
-        }
-        return _allyFactory.GetAlly(allyType) as Ally;
+        return AllyFactory.Instance.GetAlly(allyType) as Ally;
     }
 }
