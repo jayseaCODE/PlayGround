2386a7a10450172fbf62ecf7fc1807d3f286c1ad
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-16 22:40:46 +1300

    feat: change Spawner to use any creational pattern available

--- Assets/Scripts/PrototypePattern/Spawner.cs
+++ Assets/Scripts/PrototypePattern/Spawner.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum CreationalPatterns
+{
+    Singleton,
+    Factory,
+}
+
 public class Spawner : Singleton<Spawner>
 {
+    CreationalPatterns CreationalPatternToUse;
+
+    public Spawner()
+    {
+        CreationalPatternToUse = CreationalPatterns.Factory;
+    }
     public Enemy SpawnEnemy(EnemyType enemyType)
     {
-        return EnemyFactory.Instance.GetEnemy(enemyType) as Enemy;
+        switch (CreationalPatternToUse)
+        {
+            case CreationalPatterns.Singleton:
+                return EnemyFactory.Instance.GetEnemy(enemyType) as Enemy;
+            case CreationalPatterns.Factory:
+                var factory = FactoryProducer.GetFactory(FactoryType.Enemy);
+                return factory.GetEnemy(enemyType) as Enemy;
+        }
+        return null;
     }
 
     public Ally SpawnAlly(AllyType allyType)
     {
-        return AllyFactory.Instance.GetAlly(allyType) as Ally;
+        switch (CreationalPatternToUse)
+        {
+            case CreationalPatterns.Singleton:
+                return AllyFactory.Instance.GetAlly(allyType) as Ally;
+            case CreationalPatterns.Factory:
+                var factory = FactoryProducer.GetFactory(FactoryType.Ally);
+                return factory.GetAlly(allyType) as Ally;
+        }
+        return null;
     }
 }
