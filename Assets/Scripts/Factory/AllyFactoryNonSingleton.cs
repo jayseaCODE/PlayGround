ca44ece03d998c4ca8f65192de896b20e0e7b564
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-17 21:42:09 +1300

    feat: a non-singleton AllyFactory

---
+++ Assets/Scripts/Factory/AllyFactoryNonSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AllyFactoryNonSingleton : MonoBehaviour, IFactory
+{
+    public IAlly GetAlly(AllyType allyType)
+    {
+        switch (allyType)
+        {
+            case AllyType.Medic:
+                return Instantiate(Resources.Load("Medic", typeof(Ally)) as Ally);
+        }
+        return null;
+    }
+
+    public IEnemy GetEnemy(EnemyType enemyType)
+    {
+        return null;
+    }
+}
