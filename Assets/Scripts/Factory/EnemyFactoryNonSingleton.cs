b161c9ff5714426b402d6d937418508d7a3698b9
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-17 21:42:32 +1300

    feat: a non-singleton EnemyFactory

---
+++ Assets/Scripts/Factory/EnemyFactoryNonSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EnemyFactoryNonSingleton : MonoBehaviour, IFactory
+{
+    public IAlly GetAlly(AllyType allyType)
+    {
+        return null;
+    }
+    public IEnemy GetEnemy(EnemyType enemyType)
+    {
+        switch (enemyType)
+        {
+            case EnemyType.Drone:
+                return Instantiate(Resources.Load("Drone", typeof(Enemy)) as Enemy);
+            case EnemyType.Sniper:
+                return Instantiate(Resources.Load("Sniper", typeof(Enemy)) as Enemy);
+        }
+        return null;
+    }
+}
