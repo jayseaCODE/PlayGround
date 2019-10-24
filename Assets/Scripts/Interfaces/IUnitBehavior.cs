e073d3c45d99a381c263d79e9f3f966ad1dba1ca
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-24 23:25:37 +1300

    chore: update comments

--- Assets/Scripts/Interfaces/IUnitBehavior.cs
+++ Assets/Scripts/Interfaces/IUnitBehavior.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Interface to access the Strategy behavioural pattern
+/// </summary>
 public interface IUnitBehavior
 {
+    /// <summary>
+    /// Carry out this behavior
+    /// </summary>
     void Command();
 }
--- Assets/Scripts/PrototypePattern/Unit.cs
+++ Assets/Scripts/PrototypePattern/Unit.cs
@@ -9,19 +9,20 @@ public abstract class Unit : MonoBehaviour, ICopyable
 {
     protected IUnitBehavior unitBehavior;
 
+    #region Strategy behavioural pattern implementation
+    public void SetBehavior(IUnitBehavior behavior)
+    {
+        unitBehavior = behavior;
+    }
     public void ApplyBehavior()
     {
         unitBehavior.Command();
     }
+    #endregion
 
     public virtual ICopyable Copy()
     {
         return null;
     }
     public abstract ICopyable Copy(string name);
-
-    public void SetBehavior(IUnitBehavior behavior)
-    {
-        unitBehavior = behavior;
-    }
 }
