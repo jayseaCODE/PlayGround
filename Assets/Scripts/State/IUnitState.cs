d0630a8484ab40e8f1db54a131887e7f3d0347f6
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-24 23:37:18 +1300

    feat: interface to define State of a unit

---
+++ Assets/Scripts/State/IUnitState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Interface to access the State behavioural pattern
+/// </summary>
+public interface IUnitState
+{
+    /// <summary>
+    /// Carry out what happens in this state
+    /// </summary>
+    /// <param name="unit"></param>
+    void Execute(Unit unit);
+}
