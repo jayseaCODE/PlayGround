c0d643630ffa81fb3de20d973f0895721f777066
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-24 23:37:38 +1300

    feat: 2 types of state a unit can have

---
+++ Assets/Scripts/State/CollidedUnitState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidedUnitState : IUnitState
+{
+    /// <summary>
+    /// Carry out what happens when collided
+    /// </summary>
+    /// <param name="unit"></param>
+    public void Execute(Unit unit)
+    {
+        //TODO Define Collided state behavior
+    }
+}
---
+++ Assets/Scripts/State/DyingUnitState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DyingUnitState : IUnitState
+{
+    /// <summary>
+    /// Carry out what happens when dying
+    /// </summary>
+    /// <param name="unit"></param>
+    public void Execute(Unit unit)
+    {
+        //TODO Define Dying state behavior
+    }
+}
