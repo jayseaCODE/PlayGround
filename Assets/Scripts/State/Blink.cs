67678ce21b05b8534f491cc568d46e98b24d29d5
jayseaCODE <jayseaLEARN@hotmail.com>
2019-10-25 23:27:58 +1300

    feat: Blink component for when unit in Collided state

---
+++ Assets/Scripts/State/Blink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    float blinkingDuration = 0.1f; // seconds
+    new Renderer renderer;
+    Color oldColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        oldColor = renderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, blinkingDuration) / blinkingDuration;
+        renderer.material.color = Color.Lerp(oldColor, Color.white, lerp);
+    }
+}
