ed33d22fe1052b1720fa5ef26731f764874baa56
Joshua Chen <jaysealearn@hotmail.com>
2019-10-26 16:05:36 +1300

    feat: Blinking is for 2 seconds

--- Assets/Scripts/State/Blink.cs
+++ Assets/Scripts/State/Blink.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Blink : MonoBehaviour
 {
-    float blinkingDuration = 0.1f; // seconds
+    float blinkingDuration = 2.0f;
+    float currentBlinkingDuration = 0.0f;
+    float blinkingRate = 0.1f; // seconds
     new Renderer renderer;
     Color oldColor;
     // Start is called before the first frame update
@@ -17,7 +19,15 @@ public class Blink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float lerp = Mathf.PingPong(Time.time, blinkingDuration) / blinkingDuration;
-        renderer.material.color = Color.Lerp(oldColor, Color.white, lerp);
+        if (currentBlinkingDuration < blinkingDuration)
+        {
+            var lerp = Mathf.PingPong(Time.time, blinkingRate) / blinkingRate;
+            renderer.material.color = Color.Lerp(oldColor, Color.clear, lerp);
+            currentBlinkingDuration += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 }
