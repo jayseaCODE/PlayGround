5c1f9bae076c01fe3571a81bca7cba157d45867a
Joshua Chen <jaysealearn@hotmail.com>
2019-11-03 18:08:23 +1300

    refactor: GarbageCollection on entities to activate dissolve shader

--- Assets/Scripts/GarbageCollector.cs
+++ Assets/Scripts/GarbageCollector.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GarbageCollector : MonoBehaviour
 {
+    new Renderer renderer;
     // Destroy everything that enters the trigger
     void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        // Custom property from Dissolve shader used in Material DissolveRed/DissolveBlue
+        renderer = other.gameObject.GetComponent<Renderer>();
+        renderer.material.SetFloat("_StartTime", Mathf.Sin(Time.time));
+        renderer.material.SetFloat("_Animate", 1.0f);
+        StartCoroutine(Delay(other.gameObject));
+    }
+
+    IEnumerator Delay(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
